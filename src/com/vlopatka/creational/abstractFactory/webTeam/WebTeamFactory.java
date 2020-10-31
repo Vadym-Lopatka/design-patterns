43fc5c035936f7fa5a50ca1ca1cfad56e31130c4
Vadym-Lopatka <mirve09021990lik>
2020-10-31 17:19:53 +0200

    moved webTeam participants

--- src/com/vlopatka/creational/abstractFactory/webTeam/WebTeamFactory.java
+++ src/com/vlopatka/creational/abstractFactory/webTeam/WebTeamFactory.java
@@ -4,6 +4,9 @@ import com.vlopatka.creational.abstractFactory.Dev;
 import com.vlopatka.creational.abstractFactory.Pm;
 import com.vlopatka.creational.abstractFactory.Qa;
 import com.vlopatka.creational.abstractFactory.SoftwareTeamFactory;
+import com.vlopatka.creational.abstractFactory.webTeam.participants.FullstackDev;
+import com.vlopatka.creational.abstractFactory.webTeam.participants.ManualQa;
+import com.vlopatka.creational.abstractFactory.webTeam.participants.WebTeamPm;
 
 public class WebTeamFactory implements SoftwareTeamFactory {
     @Override
--- src/com/vlopatka/creational/abstractFactory/webTeam/participants/FullstackDev.java
+++ src/com/vlopatka/creational/abstractFactory/webTeam/participants/FullstackDev.java
@@ -1,4 +1,4 @@
-package com.vlopatka.creational.abstractFactory.webTeam;
+package com.vlopatka.creational.abstractFactory.webTeam.participants;
 
 import com.vlopatka.creational.abstractFactory.Dev;
 
